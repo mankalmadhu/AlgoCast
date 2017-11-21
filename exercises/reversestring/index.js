9e33b9ec6c588f45d0b3e8a9f27a0e44c7d752e8
Madhu Mankal <mankalmadhu@gmail.com>
2017-11-21 23:38:31 +0100

    more

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,18 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-   return str.split("").reverse().join("");
+   //return str.split("").reverse().join("");
+/*    let reversed = '';
+
+   for(let character of str)
+   {
+       reversed = character + reversed;
+   }
+
+   return reversed;
+ */
+   return str.split('').
+      reduce((reversed, character) => reversed = character + reversed, '');
 }
 
 module.exports = reverse;
